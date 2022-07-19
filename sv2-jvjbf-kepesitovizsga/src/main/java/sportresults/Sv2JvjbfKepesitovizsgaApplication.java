3f2c43f10b2e641356fc1de24e89edd2d1ae915a
ht0m <hartht@gmail.com>
2022-07-19 11:27:44 +0200

    Init

---
+++ sv2-jvjbf-kepesitovizsga/src/main/java/sportresults/Sv2JvjbfKepesitovizsgaApplication.java
@@ -0,0 +1,13 @@
+package sportresults;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sv2JvjbfKepesitovizsgaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sv2JvjbfKepesitovizsgaApplication.class, args);
+    }
+
+}
